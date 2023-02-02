2287c4d9d98612d487d635f47433ee6b52fb1562
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2023-02-02 17:32:33 +0100

    add missing .pxd files

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if USE_CYTHON:
 
 setup(
     name='sat-toolkit',
-    version='0.1.1',
+    version='0.1.2',
     author='Marcel Nageler',
     author_email='marcel.nageler@iaik.tugraz.at',
     description='Tool for manipulating CNF formulas',
