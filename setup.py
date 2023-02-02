5745637a21d183957cb2c0b78ef2a3e4032d8975
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2023-02-02 16:24:31 +0100

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 from setuptools import Extension, setup, find_packages
-from Cython.Build import cythonize
-from Cython.Build.Dependencies import default_create_extension
 
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
-def my_create_extension(template, kwds):
-    libs = kwds.get('libraries', []) + ["mylib"]
-    kwds['libraries'] = libs
-    kwds['include_dirs'] = ['sat_toolkit'] + kwds.get("include_dirs", [])
-    kwds['library_dirs'] = ['sat_toolkit'] + kwds.get("library_dirs", [])
-    print('#'*80)
-    print(kwds)
-    print('#'*80)
-    return default_create_extension(template, kwds)
-
-
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+extension = '.pyx' if USE_CYTHON else '.cpp'
 extensions = [
-    Extension("sat_toolkit/*", ["sat_toolkit/*.pyx"]),
+    Extension('sat_toolkit/*', ['sat_toolkit/*' + extension]),
 ]
 
+if USE_CYTHON:
+    extensions = cythonize(extensions)
+
 setup(
-    name="sat-toolkit",
-    version="0.0.1",
-    author="fanosta",
-    author_email="fanosta@users.noreply.github.com",
-    description="Tool for manipulating CNF/DNF formulas",
+    name='sat-toolkit',
+    version='0.1',
+    author='Marcel Nageler',
+    author_email='marcel.nageler@iaik.tugraz.at',
+    description='Tool for manipulating CNF formulas',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    # url="https://github.com/fanosta/sat-toolkit",
+    long_description_content_type='text/markdown',
+    url='https://github.com/fanosta/sat_toolkit',
     packages=find_packages(),
     install_requires=['numpy>=1.21'],
     ext_modules=cythonize(extensions),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
     ],
 )
