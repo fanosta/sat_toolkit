b9a8df2e9bdc2079d745e15b3a7dc8caf135d21c
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2024-03-27 15:05:42 +0100

    v0.4.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if USE_CYTHON:
 
 setup(
     name="sat-toolkit",
-    version="0.3.5",
+    version="0.4.0",
     author="Marcel Nageler",
     author_email="marcel.nageler@iaik.tugraz.at",
     description="Tool for manipulating CNF formulas",
